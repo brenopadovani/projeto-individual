d58bebbca54ae8f2e3c06573253b9d97ea21cc0d
brenopadovani <breno.leite@bandtec.com.br>
2021-11-03 22:20:36 -0300

    Site em andamento

---
+++ api-projeto-site/src/database/siteinstitucional.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE acquatec;
+
+USE acquatec;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+    descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+); 
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	temperatura DECIMAL,
+	umidade DECIMAL,
+	momento DATETIME,
+	fk_aquario INT
+);
+
+insert into usuario values
+(null,'beatriz','beatriz_fone@bandtec.com.br','1234567');
+
+select email,senha from usuario where email = 'beatriz_fone@bandtec.com.br' and senha = '1234567';
+
+select* from usuario;
